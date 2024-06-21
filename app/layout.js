9cb6c63e5abe5c2e99b34ff655eb46132a7df1f2
unknown <xinjiang1230@gmail.com>
2024-06-20 21:25:37 -0700

    Konstaintin

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Dmytro Portfolio',
+  title: 'Konstantin Portfolio',
   description: 'Created by RTR',
 }
 
