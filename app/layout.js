c191e077c291945d7edc02397ecc39aaec0c3299
XinJiang <xinjiang1230@gmail.com>
2024-06-28 11:58:36 -0700

    xinjiang

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Konstantin Portfolio',
+  title: 'XinJiang Portfolio',
   description: 'Created by RTR',
 }
 
